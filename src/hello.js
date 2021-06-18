49d7876528d776746c343be4e8bee320601e0e92
lionlemon <ame9807@gmail.com>
2021-06-18 23:45:19 +0900

    ???

---
+++ src/hello.js
@@ -0,0 +1,6 @@
+"use strict"
+//暴露一个方法
+
+exports.sayHi = function (){
+    document.write("<h1>你好，webpack1</h1>")
+}
---
+++ src/hello.ts
@@ -0,0 +1,6 @@
+export function saiHi() {
+    document.write("<h1>你好，webpack2</h1>")
+    document.write("<h1>this is a TypeScript</h1>")
+}
+
+
